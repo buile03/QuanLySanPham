40a721639a0a0a5dda7d7a5fcdaec77f7abbd56f
Bui Le <112873014+buile03@users.noreply.github.com>
2025-07-14 09:32:40 +0700

    Remove node_modules from Git tracking

--- product-management/index.js
+++ product-management/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+require('dotenv').config();
 
 const router = require("./routers/client/index.router")
 
 const app = express()
-const port = 3001
+const port = process.env.PORT;
 
 app.set('views', './views')
 app.set('view engine', 'pug')
