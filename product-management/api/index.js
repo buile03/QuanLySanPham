c700e5b3273b9df8fe81a4861a182585d9eae5b4
Bui Le <112873014+buile03@users.noreply.github.com>
2025-07-26 16:08:28 +0700

    .

--- product-management/api/index.js
+++ product-management/api/index.js
@@ -1,49 +1,8 @@
-const express = require("express");
-const path = require("path");
-require("dotenv").config();
+// Import Express app từ file index gốc (không có app.listen)
+const app = require("../index");
 
-const methodOverride = require("method-override");
-const bodyParser = require("body-parser");
-const flash = require("express-flash");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
+// Import gói serverless-http để chuyển Express thành hàm xử lý cho Vercel
+const serverless = require("serverless-http");
 
-const database = require("../config/database");
-database.connect();
-
-const systemConfig = require("../config/system");
-const routerClient = require("../routers/client/index.router");
-const routerAdmin = require("../routers/admin/index.router");
-
-const app = express();
-
-// KHÔNG dùng app.listen()
-
-// Cấu hình view engine là Pug (không dùng được trong serverless, trừ khi bạn build thành HTML sẵn hoặc dùng SSR/Next.js)
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "pug");
-
-app.locals.prefixAdmin = systemConfig.prefixAdmin;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
-
-app.use(methodOverride("_method"));
-
-app.use(cookieParser("BUILE"));
-app.use(
-  session({
-    secret: "BUILE",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 },
-  })
-);
-app.use(flash());
-
-app.use("/", routerClient);
-app.use(systemConfig.prefixAdmin, routerAdmin);
-
-// Export dưới dạng middleware handler để Vercel xử lý
-module.exports = app;
+// Export Express app dưới dạng handler để Vercel có thể gọi
+module.exports = serverless(app);
