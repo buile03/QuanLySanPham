35e1725061dcb8497f0f05e42ac9c6a0af0fcc64
Bui Le <112873014+buile03@users.noreply.github.com>
2025-07-26 14:14:35 +0700

    Chi tiết sản phẩm client

--- product-management/controllers/client/product.controller.js
+++ product-management/controllers/client/product.controller.js
@@ -1,21 +1,51 @@
 const Product = require("../../models/product.model");
 
+//index
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
+  const productsRaw = await Product.find({
     status: "active",
     deleted: false,
   }).sort({ position: "desc" });
 
-  const newProduct = products.map((item) => {
-    item.newPrice = (
-      (item.price * (100 - item.discountPercentage)) /
-      100
-    ).toFixed(0);
-    return item;
-  });
-  console.log(products);
+  const products = productsRaw.map((item) => ({
+    ...item.toObject(), // chuyển từ mongoose object sang plain JS object
+    newPrice: Math.round((item.price * (100 - item.discountPercentage)) / 100),
+  }));
+
   res.render("client/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
     products,
   });
+};
+
+// [GET] /admin/product/detail/:slug
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      slug: req.params.slug,
+      status: "active",
+    };
+
+    const productRaw = await Product.findOne(find);
+
+    if (!productRaw) {
+      return res.status(404).send("Không tìm thấy sản phẩm");
+    }
+
+    const product = {
+      ...productRaw.toObject(),
+      newPrice: Math.round(
+        (productRaw.price * (100 - productRaw.discountPercentage)) / 100
+      ),
+    };
+
+    res.render("client/pages/products/detail", {
+      pageTitle: product.title,
+      product: product,
+    });
+  } catch (err) {
+    console.error("Product List Error:", err);
+    res.status(500).send("Lỗi server");
+  }
 };
--- product-management/routers/client/product.router.js
+++ product-management/routers/client/product.router.js
@@ -1,8 +1,10 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const controller = require("../../controllers/client/product.controller")
+const controller = require("../../controllers/client/product.controller");
 
-router.get('/', controller.index);
+router.get("/", controller.index);
 
+//detail
+router.get("/detail/:slug", controller.detail);
 
 module.exports = router;
